e96af2b0b04a1c33dad20b818de48594c636110e
Forrest Kaminski <fgk55555@gmail.com>
2016-11-22 21:26:58 -0800

    Changing arduino code

--- BLDC_Test/BLDC_Test.ino
+++ BLDC_Test/BLDC_Test.ino
@@ -1,12 +1,23 @@
 #define OFF 0
+#define SEN A2
+#include "TimerOne.h"
 
 
-boolean forward = true;
-int Speed = 170;
+
+volatile int Speed = 170;
 int isReady = 0;
+int states = 7;
+int count = 0;
+volatile int noSpeedCount = 0;
+double Time = 0;
+unsigned long startTime = 0;
+unsigned long endTime = 0;
+int prevState = 7;
+volatile int rpm = 0.0;
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(22, INPUT_PULLUP); //Yellow
   pinMode(24, INPUT_PULLUP); //Green
   pinMode(26, INPUT_PULLUP); //White
@@ -18,90 +29,114 @@ void setup() {
   pinMode(3, OUTPUT); //2  Bn
   pinMode(2, OUTPUT); //1  Yp
 
+  pinMode(A2, INPUT);
+
+  Timer1.initialize(50000);
+  Timer1.attachInterrupt(defSpeed);
+  startTime = millis();
   turnOff();
 }
 
-void loop() {
-  int value1 = digitalRead(22);
-  int value2 = digitalRead(24);
-  int value3 = digitalRead(26);
-  int values[3] = {value1, value2, value3};
+void defSpeed(){
+  Speed = map(analogRead(SEN), 175, 869, 0, 200);
+  if(Speed < 0)
+    Speed = 0;
 
-  String CoilSelect = String(value1) + String(value2) + String(value3);
-  Serial.println(CoilSelect);
-  //turnOff();
+  ++noSpeedCount;
+  if(noSpeedCount >= 5){
+    rpm = 0;
+    noSpeedCount = 0;
+  }
+    
+}
+
+void loop() {
+  states = ((digitalRead(22) << 2) + (digitalRead(24) << 1) + digitalRead(26));
+  //Serial.print(states);Serial.print('\t');Serial.print(Speed);Serial.print('\t');Serial.print(rpm);Serial.print('\t');Serial.println(isReady);
+
+  if(prevState != states){
+     ++count;
+     prevState = states;
+  }
+
+  if(count >= 45){
+    endTime = millis();
+    Time = endTime - startTime;
+    Time = Time / 1000;
+    rpm = 60/Time;
+    count = 0;
+    startTime = millis();
+    noSpeedCount = 0;
+  }
+
   if(isReady > 0){
-    if (CoilSelect == "001")         //s1
-    {     analogWrite(7, OFF);  
-          analogWrite(2, OFF);
-          analogWrite(5,OFF);
-          analogWrite(3,OFF);
-          analogWrite(4, Speed);
-          analogWrite(6,Speed);
-          Serial.println(isReady);
-          delay(2);
+   switch (states) {
+    
+   case 1:
+     analogWrite(7, OFF);  
+     analogWrite(2, OFF);
+     analogWrite(5, OFF);
+     analogWrite(3, OFF);
+     analogWrite(4, Speed);
+     analogWrite(6, Speed);
+     break;
+
+
+   case 2:
+     analogWrite(4, OFF);  
+     analogWrite(6, OFF);
+     analogWrite(3, OFF);
+     analogWrite(2, OFF);
+     analogWrite(5, Speed);
+     analogWrite(7, Speed);
+     break;
+
+
+   case 3:
+     analogWrite(7, OFF);  
+     analogWrite(3, OFF);
+     analogWrite(4, OFF); 
+     analogWrite(2, OFF);
+     analogWrite(5, Speed);
+     analogWrite(6, Speed);
+     break;
+
+   case 4:
+     analogWrite(6, OFF);
+     analogWrite(5, OFF);
+     analogWrite(4, OFF);
+     analogWrite(7, OFF);
+     analogWrite(3, Speed);
+     analogWrite(2, Speed);
+     break;
+
+   case 5:
+     analogWrite(6, OFF);
+     analogWrite(5, OFF);
+     analogWrite(3, OFF);
+     analogWrite(7, OFF);
+     analogWrite(4, Speed);
+     analogWrite(2, Speed); 
+     break;
+
+   case 6:
+     analogWrite(4, OFF);  
+     analogWrite(6, OFF);
+     analogWrite(2, OFF);
+     analogWrite(5, OFF);
+     analogWrite(3, Speed);
+     analogWrite(7, Speed);
+     break;
+
+
+   case 7: 
+     turnOff();
+     break;
     }
-    else if (CoilSelect == "011")
-    {
-          analogWrite(7, OFF);  
-          analogWrite(3,OFF);
-          analogWrite(4,OFF);
-          analogWrite(2,OFF);
-          analogWrite(5,Speed);
-          analogWrite(6, Speed);
-          Serial.println(isReady);
-          delay(2);
-    }
-    else if (CoilSelect == "010")
-    {
-          analogWrite(4, OFF);  
-          analogWrite(6,OFF);
-          analogWrite(3,OFF);
-          analogWrite(2,OFF);
-          analogWrite(5, Speed);
-          analogWrite(7,Speed);
-          Serial.println(isReady);
-          delay(2);
-    }
-    else if (CoilSelect == "110")
-    {
-          analogWrite(4, OFF);  
-          analogWrite(6, OFF);
-          analogWrite(2,OFF);
-          analogWrite(5,OFF);
-          analogWrite(3,Speed);
-          analogWrite(7,Speed);
-          Serial.println(isReady);
-          delay(2);
-    }
-    else if (CoilSelect == "100")
-    { 
-          analogWrite(6, OFF);
-          analogWrite(5,OFF);
-          analogWrite(4,OFF);
-          analogWrite(7,OFF);
-          analogWrite(3, Speed); 
-          analogWrite(2,Speed);
-          Serial.println(isReady);
-          delay(2);
-    }
-    else if (CoilSelect == "101")
-    { 
-          analogWrite(6, OFF);
-          analogWrite(5,OFF);
-          analogWrite(3,OFF);
-          analogWrite(7,OFF);
-          analogWrite(4,Speed);
-          analogWrite(2, Speed); 
-          Serial.println(isReady);
-          delay(2);
-    }
-    --isReady;
+   //--isReady;
   }
-  else{
-      turnOff();
-  }
-  //delay()
+  else
+    turnOff();
 }
 
 void turnOff(){
@@ -117,7 +152,7 @@ void serialEvent(){
   while(Serial.available()) {
     char inChar = (char)Serial.read();  
     if(inChar == ' '){
-      isReady = 1000;
+      isReady = 30000;
       Serial.println("Going ");
       return;
     }
