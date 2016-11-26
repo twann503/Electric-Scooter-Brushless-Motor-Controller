7ad2eb7dbc97dfb36151ae7524eb0df6f93402ec
twann503 <thuan503@gmail.com>
2016-11-25 22:05:00 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -3,152 +3,156 @@
 #include <util/delay.h>          //This is where the delay functions are located
 #include <avr/interrupt.h>
 
-
-
 #define FOSC 16000000            // Clock Speed
 #define BAUD 9600
 #define MYUBRR FOSC/16/BAUD -1
+#define OFF 255;
 
-#define OFF 0;
+volatile uint8_t ADCvalue;
+volatile long Speed;
+uint8_t  states;
 
 
-char ReceivedChar;
-int isReady = 0;
-int states = 7;
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+void turnOff() {
+	OCR0B = OFF; //PD5, pin 7
+	OCR0A = OFF; //PD6, pin 6
+	OCR1A = OFF; //PB1, pin 5
+	OCR1B = OFF; //PB2, pin 4
+	OCR2A = OFF; //PB3, pin 3
+	OCR2B = OFF; //PD3, pin 2
+}
+
+long mapp(uint8_t x, long in_min, long in_max, long out_min, long out_max) {
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 int main(void)
 {
+	
+	int isReady = 1;
 
-	volatile int Speed = 150;
-
+	//SET pin 2-7 as OUTPUT
+	DDRB |= (1 << DDB1) | (1 << DDB2) | (1 << DDB3);	// sets port 3,4,5 as
+	DDRD |= (1 << DDD3) | (1 << DDD5) | (1 << DDD6);    // pins 2,6,7 as output
+	
+	//TIMER0
+	OCR0A = Speed;
+	OCR0B = Speed;
+	TCCR0A |= (1 << COM0A1) | (1 << COM0A0)| (1 << COM0B1) | (1 << COM0B0);	// set inverting mode
+	TCCR0A |= (1 << WGM01) | (1 << WGM00);		// set fast PWM Mode
+	TCCR0B |= (1 << CS01);						// set pre-scaler to 8 and starts PWM
+	
+	//TIMER1
+	OCR1A = Speed;
+	OCR1B = Speed;
+	TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << COM1A0) | (1 << COM1B0);	// set non-inverting mode
+	TCCR1A |= (1 << WGM10);						// set fast PWM mode 8 bit
+	TCCR1B |= (1 << WGM12);						// set fast PWM mode 8 bit
+	TCCR1B |= (1 << CS11);						// START the timer with no pre-scaler
+	
 	//SET UP FOR TIMER2 PWM PINS
-	OCR2A = 128;								// sets PWM for 50% duty cycle
-	TCCR2A |= (1 << COM2A1) | (1 << COM2B1);	// set non - inverting mode for both lines
+	OCR2B = Speed;
+	OCR2A = Speed;								// sets PWM for 50% duty cycle
+	TCCR2A |= (1 << COM2A1) | (1 << COM2B1) | (1 << COM2A0) | (1 << COM2B0) ;	// set non - inverting mode for both lines
 	TCCR2A |= (1 << WGM21) | (1 << WGM20);		// set fast PWM Mode
 	TCCR2B |= (1 << CS21);						// sets pre-scaler to 8 and starts PWM
 	
-
 	//SET UP FOR HALL EFFECT PINS
-	//sets PC2 & PC3 & PC0 as inputs for HALL_A, HALL_B, HALL_C
-	DDRC &= ~(1 << DDC2) | ~(1 << DDC3) | ~(1 << DDC0);
-	//enable pull up resistor on PC2 & PC3 & PC0 for HALL_A, HALL_B, HALL_C
-	PORTC |= (1 << PORTC2) | (1 << PORTC3) | (1 << PORTC0);
+	DDRC &= ~(1 << DDC2) | ~(1 << DDC3) | ~(1 << DDC0); //sets PC2 & PC3 & PC0 as inputs for HALL_A, HALL_B, HALL_C
+	PORTC |= (1 << PORTC2) | (1 << PORTC3) | (1 << PORTC0); //enable pull up resistor on PC2 & PC3 & PC0 for HALL_A, HALL_B, HALL_C
 	
-	
-	//SET UP FOR ANALOG ADC6 PIN READ IN
-	ADMUX = 6;				// use ADC6
-	ADMUX |= (1 << REFS0);	// use AVcc as reference voltage
-	ADMUX |= (1 << ADLAR);	// 8 bit resolution
-	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);	 // 128 prescale for 16Mhz
-	ADCSRA |= (1 << ADEN);									 // Enable the ADC
-	ADCSRA |= (1 << ADIE);									 // Enable Interrupts
-	ADCSRA |= (1 << ADSC);									 // Start the ADC conversion
-	
-	
-	//Speed = ADCH;			// read analog pin value
+	ADMUX = 6;                // use ADC6
+	ADMUX |= (1 << REFS0);    // use AVcc as the reference
+	ADMUX |= (1 << ADLAR);    // Right adjust for 8 bit resolution
+	ADCSRA |=  (1 << ADPS1) | (1 << ADPS0); // 128 prescale for 16Mhz
+	ADCSRA |= (1 << ADATE);   // Set ADC Auto Trigger Enable
+	ADCSRB = 0;               // 0 for free running mode
+	ADCSRA |= (1 << ADEN);    // Enable the ADC
+	ADCSRA |= (1 << ADSC);    // Start the ADC conversion
 
+	turnOff();
 
-	//SET UP FOR USART SERIAL CONNECTION
-	//CODE WILL ECHO SERIAL TO CONSOLE
-	
-	/*Set baud rate */
-	UBRR0H = (MYUBRR >> 8);
-	UBRR0L = MYUBRR;
-	
-	UCSR0B |= (1 << RXEN0) | (1 << TXEN0);      // Enable receiver and transmitter
-	UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);    // Set frame: 8data, 1 stp
-	
-	
-
-	while (1);
-	{
+	while (1) {
 		
-		while ( !(UCSR0A & (1 << RXC0)) )  // Wait until data is received
-		ReceivedChar = UDR0;               // Read the data from the RX buffer
+		//gets state of hall sensors
+		states = (PINC & (1 << PC2)) + (((PINC & (1 << PC3))) >> 2 ) + (PINC & (1 << PC0));
 		
-		while ( !(UCSR0A & (1 << UDRE0)) ) // Wait until buffer is empty
-		UDR0 = ReceivedChar;			   // Send the data to the TX buffer
+		//map value for throttle
+		ADCvalue = ADCH;
+		Speed = mapp(ADCvalue, 43, 215, 255, 0);
 
-		if(isReady > 0) {
+		if (Speed > 255)
+			Speed = 255;
+		else if (Speed < 0)
+			Speed = 0;
+		
+		
+		if (isReady > 0) {
 			
-						
 			switch (states) {
 				
-				case 1:
+				case 1 :
 				OCR0B = OFF; //PD5, pin 7
 				OCR1A = OFF; //PB1, pin 5
 				OCR2A = OFF; //PB3, pin 3
 				OCR2B = OFF; //PD3, pin 2
-				OCR2A = Speed; //PB2, pin 4
-				OCR0A = Speed; //PD6, pin 6 
+				OCR1B = Speed; //PB2, pin 4
+				OCR0A = Speed; //PD6, pin 6
 				break;
-	
-				case 2:
+				
+				case 2 :
 				OCR0A = OFF; //PD6, pin 6
-				OCR2A = OFF; //PB2, pin 4
+				OCR1B = OFF; //PB2, pin 4
 				OCR2A = OFF; //PB3, pin 3
 				OCR2B = OFF; //PD3, pin 2
 				OCR1A = Speed; //PB1, pin 5
 				OCR0B = Speed; //PD5, pin 7
 				break;
 				
-				case 3:
+				case 3 :
 				OCR0B = OFF; //PD5, pin 7
-				OCR2A = OFF; //PB2, pin 4
+				OCR1B = OFF; //PB2, pin 4
 				OCR2A = OFF; //PB3, pin 3
 				OCR2B = OFF; //PD3, pin 2
 				OCR1A = Speed; //PB1, pin 5
 				OCR0A = Speed; //PD6, pin 6
 				break;
 				
-				case 4:
+				case 4 :
 				OCR0B = OFF; //PD5, pin 7
 				OCR0A = OFF; //PD6, pin 6
 				OCR1A = OFF; //PB1, pin 5
-				OCR2A = OFF; //PB2, pin 4
+				OCR1B = OFF; //PB2, pin 4
 				OCR2A = Speed; //PB3, pin 3
 				OCR2B = Speed; //PD3, pin 2
 				break;
 				
-				case 5:
+				case 5 :
 				OCR0B = OFF; //PD5, pin 7
 				OCR0A = OFF; //PD6, pin 6
 				OCR1A = OFF; //PB1, pin 5
 				OCR2A = OFF; //PB3, pin 3
-				OCR2A = Speed; //PB2, pin 4
+				OCR1B = Speed; //PB2, pin 4
 				OCR2B = Speed; //PD3, pin 2
 				break;
 				
-				case 6:
+				case 6 :
 				OCR0A = OFF; //PD6, pin 6
 				OCR1A = OFF; //PB1, pin 5
-				OCR2A = OFF; //PB2, pin 4
+				OCR1B = OFF; //PB2, pin 4
 				OCR2B = OFF; //PD3, pin 2
 				OCR2A = Speed; //PB3, pin 3
 				OCR0B = Speed; //PD5, pin 7
 				break;
-				
-				case 7:
+							
+				default:
 				turnOff();
 				break;
 			}
 		}
-		else { 
-			turnOff();
-		}
-		
-	
 		//body of while loop
-		
 	}
-}
-
-void turnOff() {
-
-	OCR0B = OFF; //PD5, pin 7
-	OCR0A = OFF; //PD6, pin 6
-	OCR1A = OFF; //PB1, pin 5
-	OCR2A = OFF; //PB2, pin 4
-	OCR2A = OFF; //PB3, pin 3
-	OCR2B = OFF; //PD3, pin 2
 }
