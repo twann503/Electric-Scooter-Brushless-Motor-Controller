976fa606f51281c8f4e5ec83432ce1c45237c88e
twann503 <thuan503@gmail.com>
2016-12-05 23:51:59 -0800

    Final working code
    Added 300Hz PWM ontop of 64KHz PWM wave to control speed

--- Revision AVR Programs/Rev 1.0.c
+++ Revision AVR Programs/Rev 1.0.c
@@ -22,11 +22,9 @@ volatile int duration = 0;
 #define pin4 OCR1B
 #define pin3 OCR2A
 #define pin2 OCR2B
-
 #define Freq 300
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////
 
 void turnOff() {
 	OCR0B = OFF; //PD5, pin 7
@@ -71,52 +69,47 @@ int main(void)
 	int isReady = 1;
 
 	//SET pin 2-7 as OUTPUT
-	DDRB |= (1 << DDB1) | (1 << DDB2) | (1 << DDB3);	// sets port 3,4,5 as
+	DDRB |= (1 << DDB1) | (1 << DDB2) | (1 << DDB3);    // sets port 3,4,5 as
 	DDRD |= (1 << DDD3) | (1 << DDD5) | (1 << DDD6);    // pins 2,6,7 as output
 	
 	//TIMER0
 	TCCR0A |= (1 << COM0A1) | (1 << COM0A0)| (1 << COM0B1) | (1 << COM0B0);	// set inverting mode
 	TCCR0A |= (1 << WGM01) | (1 << WGM00);		// set fast PWM Mode
-	TCCR0B |= (1 << CS00);						// set pre-scaler to 8 and starts PWM
+	TCCR0B |= (1 << CS00);				// set pre-scaler to 8 and starts PWM
 	
 	//TIMER1
 	TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << COM1A0) | (1 << COM1B0);	// set non-inverting mode
-	TCCR1A |= (1 << WGM10);						// set fast PWM mode 8 bit
-	TCCR1B |= (1 << WGM12);						// set fast PWM mode 8 bit
-	TCCR1B |= (1 << CS10);						// START the timer with no pre-scaler
+	TCCR1A |= (1 << WGM10);		// set fast PWM mode 8 bit
+	TCCR1B |= (1 << WGM12);		// set fast PWM mode 8 bit
+	TCCR1B |= (1 << CS10);		// START the timer with no pre-scaler
 	
 	//SET UP FOR TIMER2 PWM PINS
 	TCCR2A |= (1 << COM2A1) | (1 << COM2B1) | (1 << COM2A0) | (1 << COM2B0) ;	// set non - inverting mode for both lines
 	TCCR2A |= (1 << WGM21) | (1 << WGM20);		// set fast PWM Mode
-	TCCR2B |= (1 << CS20);						// sets pre-scaler to 8 and starts PWM
+	TCCR2B |= (1 << CS20);				// sets pre-scaler to 8 and starts PWM
 	
 	//SET UP FOR HALL EFFECT PINS
 	DDRC &= ~(1 << DDC2) | ~(1 << DDC3) | ~(1 << DDC0); //sets PC2 & PC3 & PC0 as inputs for HALL_A, HALL_B, HALL_C
 	PORTC |= (1 << PORTC2) | (1 << PORTC3) | (1 << PORTC0); //enable pull up resistor on PC2 & PC3 & PC0 for HALL_A, HALL_B, HALL_C
 	
-	ADMUX = 6;                // use ADC6
-	ADMUX |= (1 << REFS0);    // use AVcc as the reference
-	ADMUX |= (1 << ADLAR);    // Right adjust for 8 bit resolution
-	ADCSRA |=  (1 << ADPS1) | (1 << ADPS0); // 128 prescale for 16Mhz
-	ADCSRA |= (1 << ADATE);   // Set ADC Auto Trigger Enable
-	ADCSRB = 0;               // 0 for free running mode
-	ADCSRA |= (1 << ADEN);    // Enable the ADC
-	ADCSRA |= (1 << ADSC);    // Start the ADC conversion
+	ADMUX = 6;               			 // use ADC6
+	ADMUX |= (1 << REFS0);    			 // use AVcc as the reference
+	ADMUX |= (1 << ADLAR);   			 // Right adjust for 8 bit resolution
+	ADCSRA |=  (1 << ADPS1) | (1 << ADPS0); 	 // 128 prescale for 16Mhz
+	ADCSRA |= (1 << ADATE);  		         // Set ADC Auto Trigger Enable
+	ADCSRB = 0;              			 // 0 for free running mode
+	ADCSRA |= (1 << ADEN); 				 // Enable the ADC
+	ADCSRA |= (1 << ADSC);   			 // Start the ADC conversion
 
 	turnOff();
-	
-	//==================================//
-	//TEST AREA//
-	
+
 	TIMSK1 = (1 << TOIE1);
 	sei();
-	
 	
 	while (1) {
 		
 		////gets state of hall sensors
 		states = (PINC & (1 << PC2)) + (((PINC & (1 << PC3))) >> 2 ) + (PINC & (1 << PC0));
-		
 		
 		ADCvalue = ADCH;
 		duration = mapp(ADCvalue, 70, 215, 0, Freq - 1);
@@ -125,8 +118,6 @@ int main(void)
 			duration = Freq - 1;
 		else if (duration < 1)
 			duration = 0;
-		
-		
 		
 		if (isReady > 0) {
 			
@@ -181,19 +172,15 @@ int main(void)
 				pin6 = OFF;   //RN
 				pin7 = OFF;   //YN
 				pin4 = OFF;   //BP
-				pin2 = OFF; //YP
+				pin2 = OFF;   //YP
 				pin5 = Speed; //BN
-				pin3 = Speed;   //RP
+				pin3 = Speed; //RP
 				break;
 				
 				default:
 				turnOff();
 				break;
-				
 			}
 		}
-		//else
-		//turnOff();
-		//body of while loop
 	}
 }
